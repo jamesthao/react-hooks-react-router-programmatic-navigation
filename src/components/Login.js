e90148b8bba1ad8e34096c33870c7ab0e555909a
James <jamesthao32@gmail.com>
2022-06-20 01:53:48 -0600

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,8 +20,7 @@ function Login({ setIsLoggedIn }) {
 
     setIsLoggedIn(true);
 
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    history.push("/home");
   }
 
   return (
